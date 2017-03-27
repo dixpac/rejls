8112db2abac992d4bb7ad3b5ca2dc5ffeb312207
dixpac <dino.onex@gmail.com>
2017-03-27 16:25:29 +0200

    Add route url helpers

--- test/action_view_test.rb
+++ test/action_view_test.rb
@@ -68,4 +68,12 @@ class ActiveViewTest < Minitest::Test
     assert_match "<h1>Post A</h1>", response.body
     assert_match "</html>", response.body
   end
+
+  def test_render_index
+    request = Rack::MockRequest.new(Rails.application)
+
+    response = request.get("/posts")
+
+    assert_match "<h1>The Blog</h1>", response.body
+  end
 end
